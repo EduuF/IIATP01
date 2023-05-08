0decf9d2cdb98e2412d124780bc23bc6f87932f5
fiuza <fiuza@technium.me>
2023-05-07 23:23:23 -0300

    V3.3.4 - Try: Except: no main

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from utils.algorithm_manager import *
 if __name__ == '__main__':
 
     selected_algorithm, elements, print_results = get_inputs()
-    alg_instance = algorithm_manager(selected_algorithm, elements)
+
+    try:
+        alg_instance = algorithm_manager(selected_algorithm, elements)
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
 
     alg_instance.set_print_results(print_results)
     alg_instance.sort_elements()
