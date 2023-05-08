4594cc940f6a6bd2be5147f9e70974b4a38d230d
fiuza <fiuza@technium.me>
2023-05-07 23:20:41 -0300

    V3.3.1 - Dicionario no algorithm_manager.py

--- utils/algorithm_manager.py
+++ utils/algorithm_manager.py
@@ -5,22 +5,16 @@ from src.algorithms.GBFS import *
 from src.algorithms.AStar import *
 
 def algorithm_manager(algorithm, elements):
-    if algorithm == 'B':
-        bfs_instance = BFS(elements)
-        return bfs_instance
+    algoritms = {
+        'B': BFS,
+        'I': IDS,
+        'U': UCS,
+        'A': AStar,
+        'G': GBFS
+    }
 
-    if algorithm == 'I':
-        ids_instance = IDS(elements)
-        return ids_instance
+    if algorithm in algoritms:
+        return algoritms[algorithm](elements)
+    else:
+        raise ValueError("Error: '{}' is not a valid algorithm.".format(algorithm))
 
-    if algorithm == 'U':
-        ucs_instance = UCS(elements)
-        return ucs_instance
-
-    if algorithm == 'A':
-        a_star_instance = AStar(elements)
-        return a_star_instance
-
-    if algorithm == 'G':
-        gbfs_instance = GBFS(elements)
-        return gbfs_instance
